a01daa76a80453323d2defea9224ed20ce5b4b47
MuhamadIsmuaji <muhamadismuaji@gmail.com>
2016-10-31 07:01:45 +0700

    basic layouting

---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app2')
+
+@section('content')
+	<h1>Users</h1>
+	<table border="1" cellspacing="0">
+		<thead>
+			<tr>
+				<th>No</th>
+			</tr>
+		</thead>
+	</table>
+@endsection()
---
+++ resources/views/layouts/app2.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Laravel</title>
+</head>
+<body>
+
+	<header>
+		<h1>This is header</h1>
+		<hr>
+	</header>
+	
+	<div class="content">
+		@yield('content')
+	</div>
+
+</body>
+</html>
